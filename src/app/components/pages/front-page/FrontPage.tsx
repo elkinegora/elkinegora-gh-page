f86ebd1a64defbe0a00bc5fa34a559a75201f102
Egor Elkin <elkinegora@gmail.com>
2018-10-30 01:57:55 +0500

    redesigned classes of components, finished setting the environment of frontpage

---
+++ src/app/components/pages/front-page/FrontPage.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+export default class MainPage extends React.Component<MainPageProps, {}> {
+
+    render(){
+        return(
+            <div className="frontpage__container">
+                <h1>Hello</h1>
+            </div>
+        );
+    }
+}
